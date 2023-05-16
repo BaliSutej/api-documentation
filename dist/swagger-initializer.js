5e28dc7d8d2b8c570b61e93e888cc3f40a2d0c69
sutej bali <sutejbali@gmail.com>
2023-05-16 17:45:07 +0530

    feat: nested swagger docs

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "api-swagger/pet.yaml",
+    url: "api-swagger/openapi.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
